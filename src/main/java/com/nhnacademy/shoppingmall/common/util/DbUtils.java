0a4f59bcf46b1a1f92e6181b8570e7ac2ae741a2
taeyungithub <rlaxodbs629@naver.com>
2024-10-22 20:28:27 +0900

    todo#1

--- src/main/java/com/nhnacademy/shoppingmall/common/util/DbUtils.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/util/DbUtils.java
@@ -23,18 +23,23 @@ public class DbUtils {
         }
 
         //todo#1-1 {ip},{database},{username},{password} 설정
-
+        basicDataSource.setUrl("jdbc:mysql://133.186.241.167:3306/nhn_academy_19");
+        basicDataSource.setUsername("nhn_academy_19");
+        basicDataSource.setPassword("2/5l(ky6VP_bbyzZ");
 
         //todo#1-2 initialSize, maxTotal, maxIdle, minIdle 은 모두 5로 설정합니다.
-
+        basicDataSource.setInitialSize(5);
+        basicDataSource.setMaxTotal(5);
+        basicDataSource.setMaxIdle(5);
+        basicDataSource.setMinIdle(5);
 
         //todo#1-3 Validation Query를 설정하세요
-
+        basicDataSource.setValidationQuery("select 1");
 
         basicDataSource.setMaxWait(Duration.ofSeconds(2));
 
         //todo#1-4 적절히 변경하세요
-        DATASOURCE = null;
+        DATASOURCE = basicDataSource;
 
     }
 
