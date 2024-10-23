d1cf0dea21ef2e70f2f96e8739e3bbac61fb1424
taeyungithub <rlaxodbs629@naver.com>
2024-10-23 15:33:22 +0900

    todo#9

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
@@ -17,6 +17,10 @@ public class WelcomePageFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#9 /요청이 오면 welcome page인 index.do redirect 합니다.
-
+        if (req.getRequestURI().equals("/")) {
+            res.sendRedirect(req.getContextPath() + "/index.do");
+        } else {
+            chain.doFilter(req, res);
+        }
     }
 }
