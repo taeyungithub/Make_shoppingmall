2c10e1b0cdb69a904c0565c953b0b487f656075f
taeyungithub <rlaxodbs629@naver.com>
2024-10-23 16:38:27 +0900

    todo#10

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @Slf4j
@@ -14,6 +15,15 @@ public class LoginCheckFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#10 /mypage/ 하위경로의 접근은 로그인한 사용자만 접근할 수 있습니다.
+        String requestURI = req.getRequestURI();
 
+        if (requestURI.startsWith("/mypage/")) {
+            Object user = req.getSession().getAttribute("user");
+            if (user != null) {
+                res.sendRedirect("/login.do");
+                return;
+            }
+        }
+        chain.doFilter(req, res);
     }
 }
